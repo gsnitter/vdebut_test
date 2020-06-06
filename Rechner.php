a2133ae27295f6237edfb3ac5f3095c97643ae6f
Steffen Nitter <steffen_nitter@gmx.de>
2020-06-06 16:25:51 +0200

    Leider doch noch einige Probleme identifiziert: Zwar funktinoiert step over und step in, aber z.B. nicht run to cursor. Mal das xdebug. doch noch einige Probleme identifiziert: Zwar funktinoiert step over und step in, aber z.B. nicht run to cursor. Mal das xdebug.log erstellen lassen.log erstellen lassen.

--- Rechner.php
+++ Rechner.php
@@ -4,6 +4,8 @@ class Rechner
 {
     public function getUmfangByRadius(float $radius): float
     {
+        $radius = $radius * 2;
+        $radius = $radius / 2;
         return 2 * pi() * $radius;
     }
 }
